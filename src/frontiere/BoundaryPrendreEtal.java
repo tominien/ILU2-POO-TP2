9826093f33d0a3e6d2fd650111df02f839b2ce10
tominien <tom.leber.toulouse@gmail.com>
2025-04-03 18:59:31 +0200

    0.0.4 - Cas PrendreEtal fini

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -10,10 +10,28 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (nomVendeurConnu) {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.");
+			if (controlPrendreEtal.resteEtals()) {
+				installerVendeur(nomVendeur);
+			} else {
+				System.out.println("Désolée " + nomVendeur + " je n'ai plus  d'étal qui ne soit pas déjà occupé.");
+			}
+		} else {
+			System.out.println("Je suis désolée " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un étal pour vous ! Il me faudrait quelques renseignements :");
+
+		String produit = Clavier.entrerChaine("Quels produits souhaitez-vous vendre ?");
+		int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if (numeroEtal != -1) {
+			System.out.println("le vendeur " + nomVendeur + "s'est installé à l'étal n°" + numeroEtal);
+		}
 	}
 }
