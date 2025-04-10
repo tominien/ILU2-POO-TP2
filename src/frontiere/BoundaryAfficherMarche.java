6100c1dfbddc1e154d375ed0401eea5d8fc3537c
tominien <tom.leber.toulouse@gmail.com>
2025-04-10 11:23:21 +0200

    0.0.7 - Cas AfficheMarche fini

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,20 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			System.out.println(nomAcheteur + ", vous trouverez au marché :");
+			String vendeur, quantite, produit;
+			for (int i = 0; i < infosMarche.length; i++) {
+				vendeur = infosMarche[i];
+				i++;
+				quantite = infosMarche[i];
+				i++;
+				produit = infosMarche[i];
+				System.out.println("- " + vendeur + " qui vend " + quantite + " " + produit);
+			}
+		}
 	}
 }
